5a3e83b6cb65344dbac4ec417f1334e1af31e0cd
Nick <nicksaurinocruz@gmail.com>
2024-03-05 17:25:39 -0500

    Practicing object oriented programming

---
+++ src/test/java/com/ufostyle/poo/PooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufostyle.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
