5c07dbf451b896b3fe37b3522e18d52edcf857a6
Nick <nicksaurinocruz@gmail.com>
2024-03-05 18:10:51 -0500

    Creating methods

--- src/main/java/com/ufostyle/poo/domain/entities/Alumno.java
+++ src/main/java/com/ufostyle/poo/domain/entities/Alumno.java
@@ -5,4 +5,24 @@ public class Alumno {
   private Integer alumnoId;
   private String nombres;
   private String apellidos;
+
+  /**
+   * Se implemento este método
+   */
+  public void mostrarNombre() {
+    System.out.println("Hola, soy un alumno y se decir mi nombre");
+  }
+
+  /**
+   * Se implento este método
+   * @param calificacion
+   */
+  public void saberAprobado(Double calificacion) {
+    if (calificacion >= 13) {
+      System.out.println("Aprobé la materia");
+    }
+    else {
+      System.out.println("Uyyyyy no aprobé");
+    }
+  }
 }
